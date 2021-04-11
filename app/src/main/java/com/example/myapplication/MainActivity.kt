c3da9b27625e2ebdb524c584cd547a4d8f32f2f5
dotroof <dotroof@gmail.com>
2021-04-11 14:38:40 +0900

    LiveDataのテスト実装

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
@@ -13,13 +14,25 @@ class MainActivity : AppCompatActivity() {
 
         val viewModel: MainViewModel= ViewModelProviders.of(this).get(MainViewModel::class.java)
 
+        val button = findViewById<TextView>(R.id.button)
         val text1 = findViewById<TextView>(R.id.text1)
+        val text2 = findViewById<TextView>(R.id.text2)
+
+        button.setOnClickListener {
+            viewModel.pushCount.value = viewModel.pushCount.value!! + 1
+        }
+
         if (viewModel.rotationCount == 0) {
             text1.text = viewModel.text
         } else {
             text1.text = viewModel.rotationCount.toString()
         }
 
+        val pushCountObserver = Observer<Int> { pushCount ->
+            text2.text = pushCount.toString()
+        }
+
         viewModel.rotationCount++
+        viewModel.pushCount.observe(this, pushCountObserver)
     }
 }
--- app/src/main/java/com/example/myapplication/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/MainViewModel.kt
@@ -1,8 +1,17 @@
 package com.example.myapplication
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     val text: String = "Hello World!"
     var rotationCount: Int = 0
+
+    val pushCount: MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>()
+    }
+
+    init {
+        pushCount.value = 0
+    }
 }
